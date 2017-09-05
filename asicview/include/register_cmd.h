ee7a330685c553c2cdff9c840bf431ba085fb194
Sagar Borikar <sborikar@pensando.io>
2017-09-05 10:31:05 -0700

    update

--- asicview/include/register_cmd.h
+++ asicview/include/register_cmd.h
@@ -5,12 +5,18 @@
 #ifndef __REGISTER_CMD__H__
 #define __REGISTER_CMD__H__
 
+#include <map>
+using namespace std;
+
 typedef int (*cmd_handler_t)(int argc, char *argv[]);
 
 struct cmd_info {
     char *cmd_name;
     cmd_handler_t cmdf;
 };
+
+char *match(const char *text, int state);
+int asicview_cli(void);
 
 #define REGISTER_CMD(name, func) \
     static struct cmd_info __cmd__##func \
@@ -19,4 +25,6 @@ struct cmd_info {
             .cmd_name = #name,  \
             .cmdf = func,    \
         };
+
+void get_cmd_list(char **ptr);
 #endif
